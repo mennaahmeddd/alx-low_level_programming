4a94ff6168f52813ec565b05e0d10f76b9b93a6c
mennaahmeddd <manooahmed418@gmail.com>
2023-03-16 09:22:48 -0700

    c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+/**
+ * main -Entry point
+ * 
+ *Description : A C program that using puts function
+ */
 int main(void)
 {
     puts("\"Programming is like building a multilingual puzzle");
