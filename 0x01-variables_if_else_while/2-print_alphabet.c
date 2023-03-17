afdff99218e4f35f4969c63ec8fd3d869d9b2612
mennaahmeddd <manooahmed418@gmail.com>
2023-03-17 12:54:11 -0700

    c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,7 +8,8 @@ int main(void)
 {
 	char lowercase;
 
-	for(lowercase = 'a'; lowercase <= 'z'; lowercase++){
+	for(lowercase = 'a'; lowercase <= 'z'; lowercase++)
+	{
 	              putchar(lowercase);
 		      putchar('\n');
 	}
