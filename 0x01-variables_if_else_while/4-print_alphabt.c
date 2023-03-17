ec6d2e0f62dfc8852775ab59dd4ceb529b1a82df
mennaahmeddd <manooahmed418@gmail.com>
2023-03-17 13:11:18 -0700

    c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char lowercase;
+
+	for (lowercase = 'a'; lowercase <= 'z'; lowercase++)
+	{
+		if(lowercase != 'e' && lowercase != 'q')
+		{
+                    	putchar(lowercase);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
