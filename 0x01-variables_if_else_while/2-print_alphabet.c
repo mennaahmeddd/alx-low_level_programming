272104879b5b8d2a36befc2277fc20381795324e
mennaahmeddd <manooahmed418@gmail.com>
2023-03-17 12:48:12 -0700

    c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int lowercase;
+
+	for(lowecase = 'a'; lowercase <= 'z'; lowercase++){
+	              putchar(lowercae);
+		      putchar('\n');
+	}
+	return (0);
+}
