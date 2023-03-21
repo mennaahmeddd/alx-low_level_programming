9b17a8cb3cae6d403d20c9f624b67d3d0e336705
mennaahmeddd <manooahmed418@gmail.com>
2023-03-21 16:58:36 -0700

    c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - Add two numbers
+ *
+ * @a: first input
+ * @b: second input
+ *
+ * Return: Sum of a and b
+ */
+int add(int a, int b)
+{
+	int sum = a + b;
+
+	return (sum);
+}
---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * print_to_98 - Prints n to 98, followed by a new line
+ * @n: A input integer
+ */
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		for (; n <= 98; n++)
+		{
+			printf("%d", n);
+
+			if (n == 98)
+				continue;
+			printf(", ");
+		}
+		printf("\n");
+	}
+	else
+	{
+		for (; n >= 98; n--)
+		{
+			printf("%d", n);
+
+			if (n == 98)
+				continue;
+			printf(", ");
+		}
+		printf("\n");
+	}
+}
