5dd1fc03db95a4ada45a17527f0ee77aed2dfd41
mennaahmeddd <manooahmed418@gmail.com>
2023-03-23 15:24:37 -0700

    c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_diagonal - Draws a diagnonal line
+ * @n: An input integer
+ * Return: Always 0 (Success)
+ */
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < i; j++)
+				_putchar(' ');
+		_putchar(92);
+		_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
