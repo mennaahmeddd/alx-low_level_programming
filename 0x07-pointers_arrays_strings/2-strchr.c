631d74a81874b50654edbab106ecd6011a5cc1a0
mennaahmeddd <manooahmed418@gmail.com>
2023-04-03 12:30:28 -0700

    c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -7,14 +7,15 @@
  */
 char *_strchr(char *s, char c)
 {
+	int i;
 
-	while (*s)
+	for (i = 0; s[i] >= '\0'  ; i++)
 	{
-		if (c == *s)
-			return (s);
-		s++;
+		if (s[i] == c)
+		{
+			return (s + i);
+		}
 	}
-	if (c == *s)
-		return (s);
-	return (NULL);
+
+	return ('\0');
 }
